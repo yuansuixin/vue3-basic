d5c67496d8143df0697230d7c728facc6f655ac5
沐清（马草原） <muqing@micous.com>
2021-02-22 14:52:48 +0800

    feat ref

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>{{ count }}</h1>
+  <h1>{{ double }}</h1>
+  <button @click="increase">+1</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent, computed, reactive, toRefs } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+interface DataProps {
+  count: number
+  double: number
+  increase: () => void
+}
 export default defineComponent({
   name: 'App',
+  setup() {
+    //   const count = ref(0)
+    //   const double = computed(() => {
+    //     return count.value * 2
+    //   })
+    //   const increase = () => {
+    //     count.value++
+    //   }
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => {
+        data.count++
+      },
+      double: computed(() => data.count * 2),
+    })
+    //变成响应式
+    const refData = toRefs(data)
+    return {
+      ...refData,
+    }
+  },
   components: {
-    HelloWorld
-  }
-});
+    HelloWorld,
+  },
+})
 </script>
 
 <style>
